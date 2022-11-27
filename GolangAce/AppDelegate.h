21940c7abdcf43375cfaf5048264d66a158f3035
ASNTHEGREAT <aditya.nittala5@gmail.com>
2022-11-27 19:51:43 +0530

    Initial Commit

---
+++ GolangAce/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  GolangAce
+//
+//  Created by Aditya Nittala on 27/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ GolangAce/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  GolangAce
+//
+//  Created by Aditya Nittala on 27/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
